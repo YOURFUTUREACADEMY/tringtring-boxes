fe406c5e0111e76c01c30387261abfc00ddddf75
Daniel <daniel@web-reality.nl>
2020-04-01 14:21:27 +0200

    added store links

--- source/js/applink.js
+++ source/js/applink.js
@@ -3,7 +3,7 @@ const appLink = document.querySelectorAll(".application");
 function getAppLink(type) {
     for (i = 0; i < appLink.length; i++) {
         if (type == "apple") {
-            appLink[i].href = "https://www.apple.com/ios/app-store/";
+            appLink[i].href = "https://apps.apple.com/app/id1505493471";
         }
         if (type == "android") {
             appLink[i].href = "https://play.google.com";
