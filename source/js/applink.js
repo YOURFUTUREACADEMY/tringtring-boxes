d5b61403d9645df031035bb1d164f8cc0759dd09
Daniel <daniel@web-reality.nl>
2020-04-02 20:50:24 +0200

    images added fix js for platform

--- source/js/applink.js
+++ source/js/applink.js
@@ -1,12 +1,22 @@
-const appLink = document.querySelectorAll(".application");
+const appLink = document.querySelectorAll('.application');
 
 function getAppLink(type) {
     for (i = 0; i < appLink.length; i++) {
-        if (type == "apple") {
-            appLink[i].href = "https://apps.apple.com/app/id1505493471";
+
+
+        switch (type) {
+            case 'apple':
+                appLink[i].href = 'https://apps.apple.com/app/id1505493471';
+                break;
+            case 'android':
+                appLink[i].href = 'https://play.google.com/store/apps/details?id=nl.tringtring.bestellen.foodmarketamsterdam';
+            case 'iOS':
+                appLink[i].href = 'https://apps.apple.com/app/id1505493471';
+                break;
+            default:
+                appLink[i].href = '#downloadApp';
+                break;
         }
-        if (type == "android") {
-            appLink[i].href = "https://play.google.com/store/apps/details?id=nl.tringtring.bestellen.foodmarketamsterdam";
-        }
+
     }
 }
