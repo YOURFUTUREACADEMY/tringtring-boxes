d9e6c3f11ef2112cab56b7d9547fea1584d23522
Daniel <daniel@web-reality.nl>
2020-04-10 18:04:56 +0200

    added eleventy first version

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,24 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./src/img");
-    eleventyConfig.addPassthroughCopy("./src/css");
-    eleventyConfig.addPassthroughCopy("./src/fonts");
-    eleventyConfig.addPassthroughCopy("./src/js");
-    eleventyConfig.addPassthroughCopy("./src/*.png");
-    eleventyConfig.addPassthroughCopy("./src/*.ico");
-    eleventyConfig.addPassthroughCopy("./src/robots.txt");
-    eleventyConfig.addPassthroughCopy("./src/manifest.json");
+    eleventyConfig.addPassthroughCopy('./src/img');
+    eleventyConfig.addPassthroughCopy('./src/css');
+    eleventyConfig.addPassthroughCopy('./src/fonts');
+    eleventyConfig.addPassthroughCopy('./src/js');
+    eleventyConfig.addPassthroughCopy('./src/*.png');
+    eleventyConfig.addPassthroughCopy('./src/*.ico');
+    eleventyConfig.addPassthroughCopy('./src/robots.txt');
+    eleventyConfig.addPassthroughCopy('./src/manifest.json');
+
+    eleventyConfig.addLayoutAlias('homePage', 'templates/homePage.njk');
+    eleventyConfig.addLayoutAlias('voorwPrivacy', 'templates/voorwPrivacy.njk');
+
 
     return {
+        markdownTemplateEngine: 'njk',
         dir: {
             input: 'src',
+            layouts: '_layouts',
+            includes: '_includes',
             output: 'dist'
         }
-    }
-}
+    };
+};
